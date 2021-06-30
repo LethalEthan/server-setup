8ace327a4cc5b500357dceabd5f0995193b28416
Ethan <ethanjhem.e@gmail.com>
2021-06-30 21:15:39 +0100

    Update dependencies

--- setup.bat
+++ setup.bat
@@ -2,6 +2,8 @@
 python -m pip install --upgrade pip
 pip3 install psutil
 pip3 install requests
+pip3 install pyyaml
+pip3 install subprocess
 echo "Warning! if modules cannot be found please check that you do not have multiple python versions!"
 python setup.py
 pause
